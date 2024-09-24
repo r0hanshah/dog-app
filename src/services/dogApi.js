97ea93cc0a0a43591b34bd2ffec84bd3ee7eef25
Rohan Shah <rshah04@gmail.com>
2024-09-24 16:15:42 -0400

    created functions to handle api calls

--- src/services/dogApi.js
+++ src/services/dogApi.js
@@ -1,2 +1,16 @@
 import axios from 'axios';
 
+const API_BASE_URL = 'https://dog.ceo/api';
+
+
+export const getBreedsList = async () => {
+    return await axios.get(`${API_BASE_URL}/breeds/list/all`);
+  };
+  
+  export const getBreedImages = async (breed) => {
+    return await axios.get(`${API_BASE_URL}/breed/${breed}/images`);
+  };
+  
+  export const getSubBreedImages = async (breed, subBreed) => {
+    return await axios.get(`${API_BASE_URL}/breed/${breed}/${subBreed}/images`);
+  };
